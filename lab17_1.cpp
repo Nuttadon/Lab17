bee329a6244c21987dd3bd461fba30ea2cd5a43c
Nuttadon <nuttadon_p@cmu.ac.th>
2022-01-18 16:23:50 +0700

    Lab17_1 complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,39 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *x,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(x+i);
+        if(i>=0&&i%M==M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *y,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(y+i) = (rand()%101)/100.00 ;
+    }
+}
+
+void findRowSum(const double *inp,double *sum,int N,int M){
+	int remem=0;
+	for(int i=0;i<N;i++){
+		sum[i] = 0; 
+		for(int j=0;j<M;j++){
+			*(sum+i)+= *(inp+remem);
+			remem++;
+		}
+	}
+}
+
+void findColSum(const double *inp,double *sum,int N,int M){
+	for(int i=0;i<M;i++){
+		sum[i] = 0; 
+		for(int j=0;j<N;j++){
+			*(sum+i) +=  *(inp+i+M*j);
+		}
+	}
+}
+
